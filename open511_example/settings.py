e407581af4e8f1cb7161905e85134bbd238be357
Michael Mulley <michael@michaelmulley.com>
2016-04-11 14:53:01 -0400

    Django upgrade changes

--- open511_example/settings.py
+++ open511_example/settings.py
@@ -8,23 +8,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+import os
+
 OPEN511_BASE_URL = 'http://localhost:8000'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c)9ib3z3avntmnd0t(3&vkbd5m=dn30csp8*yjvkg^14t+5!81'
+SECRET_KEY = 'thisisinsecure'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -53,12 +52,29 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware'
 )
 
 ROOT_URLCONF = 'open511_example.urls'
 
 WSGI_APPLICATION = 'open511_example.wsgi.application'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 
 # Database
--- open511_example/urls.py
+++ open511_example/urls.py
@@ -1,21 +1,20 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
 
     url(r'^api/', include('open511_server.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'open511_example.hello.simple_index_page'),
     
-)
+]
 
 try:
     import django_open511_ui
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^map/', include('django_open511_ui.urls')),
         url(r'^accounts/', include('django_open511_ui.auth_urls')),
-    )
+    ]
 except ImportError:
     pass
